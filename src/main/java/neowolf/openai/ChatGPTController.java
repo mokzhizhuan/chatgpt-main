3f1b0bf0d3cb2f1f98fd481598264ec6cfdf04ee
MokZhizhuan(NeoWolf) <53340265+mokzhizhuan@users.noreply.github.com>
2023-11-06 18:37:56 +0800

    Update ChatGPTController.java

--- src/main/java/neowolf/openai/ChatGPTController.java
+++ src/main/java/neowolf/openai/ChatGPTController.java
@@ -77,4 +77,24 @@ public class ChatGPTController {
 
 		return response.trim();
 	}
+
+	/*	@ResponseBody
+	@PostMapping("/chat")
+	public String chat(@ModelAttribute("chatGPTBean") ChatGPTBean chatGPTBean, Model model, HttpSession session) {
+		log.info("Getting answer from Open AI ChatGPT");
+		String prompt = chatGPTBean.getPrompt(), response = "Fatal Error";
+		try {
+			String currentChat = (String) session.getAttribute("currentChat");
+			currentChat = (currentChat == null || "".equals(currentChat) ? "" : currentChat) + "\nQ: " + prompt
+					+ "\nA: ";
+			currentChat += chatGPTService.parseUnstructuredData(prompt).trim();
+			response = currentChat;
+			model.addAttribute("currentChat", currentChat);
+		} catch (Exception e) {
+			e.printStackTrace();
+			response = messageSource.getMessage("apiCallfailed", new String[] { e.getMessage() }, locale);
+		}
+
+		return response.trim();
+	}*/
 }
